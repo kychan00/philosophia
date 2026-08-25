2dc0deb2d1dea26f11c0216756fa54b6b5fc4b6e
Kychan <kychan@MacBook-Air-de-Kychan.local>
2026-08-25 14:56:02 -0600

    Add complete Leibniz study system and Frankfurt reading task

--- src/data/tasks.js
+++ src/data/tasks.js
@@ -189,6 +189,20 @@ export const tasks = [
     studyRoute: '/tareas/ontologia-ii/leibniz-discurso-monadologia',
   },
 
+  {
+    id: 'teoria-critica-2026-08-25-programa-reale-antiseri',
+    subject: 'Teoría Crítica',
+    code: 'FI265',
+    assigned: '2026-08-25',
+    due: '2026-08-25',
+    title: 'La Teoría Crítica de la Escuela de Frankfurt',
+    type: 'Lectura / estudio',
+    weight: null,
+    description:
+      'Leer el programa de la materia y la lectura de Reale y Antiseri sobre la Teoría Crítica de la Escuela de Frankfurt.',
+    sourceRoute: '/semestre/5/teoria-critica',
+    deadline: '17:25',
+  },
 ]
 
 export default tasks
